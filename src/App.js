e1bade22c6e1fad51a280328982e1899e3823a3f
Prasad-K-Kandalkar <prasadkandalkar220802@gmail.com>
2023-07-05 23:04:37 +0530

    ForgotPassword,ResetPassword,VerifyPassword Pages created

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import OpenRoute from "./components/core/Auth/OpenRoute"
 
 import Login from "./pages/Login"
 import Signup from "./pages/Signup"
+import ForgotPassword  from "./pages/ForgotPassword";
+import UpdatePassword from "./pages/UpdatePassword";
+import VerifyEmail from "./pages/VerifyEmail";
 
 function App() {
   return (
@@ -29,6 +32,39 @@ function App() {
             </OpenRoute>
           }
         />
+        <Route
+          path="forgot-password"
+          element={
+            <OpenRoute>
+              <ForgotPassword />
+            </OpenRoute>
+          }
+        />
+        <Route
+          path="update-password/:id"
+          element={
+            <OpenRoute>
+              <UpdatePassword />
+            </OpenRoute>
+          }
+        />  
+         <Route
+          path="verify-email"
+          element={
+            <OpenRoute>
+              <VerifyEmail />
+            </OpenRoute>
+          }
+        />
+
+    {/* <Route
+          path="about"
+          element={
+            <OpenRoute>
+              <About />
+            </OpenRoute>
+          }
+        />   */}
     </Routes>
 
    </div>
